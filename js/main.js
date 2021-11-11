2df699862d6b1edb35f232204282cca057bdf071
Mina Amir <mina.amir25@yahoo.com>
2021-11-11 21:48:11 +0200

    add momentjs library to help manipulate dates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// Helper function to transform data from string to object
+function getArrayOfObjectsData(array) {
+	let arrayOfData = [];
+	for (let i = 0; i < array.length; i++) {
+		let singleRowData = array[i];
+		singleRowData = singleRowData.split(',');
+		let rowDataObject = {
+			empID: singleRowData[0],
+			projectID: singleRowData[1],
+			dateFrom: singleRowData[2],
+			dateTo: singleRowData[3].toLowerCase() === 'null' ? moment().format('YYYY-MM-DD') : singleRowData[3],
+		};
+		arrayOfData.push(rowDataObject);
+	}
+	return arrayOfData;
+}
+
 // Receive the text file from user input
 async function loadFile(e) {
 	let file = e.target.files[0];
